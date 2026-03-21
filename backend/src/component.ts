9ddbbe75626603b63da62004ce056e130a7064f0
Julian Wright <PepperLola@users.noreply.github.com>
2026-03-21 04:35:32 -0700

    fix: forgot to stage backend, needed to increase size of some decimals

--- backend/src/component.ts
+++ backend/src/component.ts
@@ -7,7 +7,23 @@ app.get('/', async c => {
     return c.json(await sql`SELECT * FROM component`)
 })
 
-app.post('/create', async c => {
+app.get('/components', async c => {
+    const res = await sql`SELECT component.part_num, *,
+        CASE
+            WHEN capacitor.part_num = component.part_num THEN 'Capacitor'
+            WHEN resistor.part_num = component.part_num THEN 'Resistor'
+            WHEN diode.part_num = component.part_num THEN 'Diode'
+            ELSE NULL END AS component_type
+    FROM component
+    LEFT JOIN capacitor USING (part_num)
+    LEFT JOIN resistor USING (part_num)
+    LEFT JOIN diode USING (part_num);`
+
+    return c.json(res);
+});
+
+app.post('/component/:component_type', async c => {
+    const { component_type } = c.req.param()
     const body = await c.req.json()
 
     const {
@@ -25,10 +41,6 @@ app.post('/create', async c => {
         supplier_name
     } = body;
 
-    await sql`INSERT INTO supplier (name, url, country) VALUES ('DigiKey', 'https://digikey.com', 'US') ON CONFLICT DO NOTHING;`
-
-    await sql`INSERT INTO location (storage_name, position, facility) VALUES (${storage_name}, ${position}, ${facility}) ON CONFLICT DO NOTHING;`;
-
     await sql`
 INSERT INTO component (
     part_num,
@@ -45,36 +57,43 @@ INSERT INTO component (
     supplier_name
     ) VALUES (
     ${part_num},
-    ${price},
+    ${price ?? null},
     ${name},
-    ${pkg},
-    ${tolerance},
-    ${quantity},
-    ${voltage_rating},
-    ${additional},
-    ${storage_name},
-    ${position},
-    ${facility},
+    ${pkg ?? null},
+    ${tolerance ?? null},
+    ${quantity ?? null},
+    ${voltage_rating ?? null},
+    ${additional ?? null},
+    ${storage_name ?? null},
+    ${position ?? null},
+    ${facility ?? null},
     ${supplier_name}
     );`
 
+    switch (component_type) {
+        case "resistor":
+            const { power, resistance, composition } = body
+            await sql`
+            INSERT INTO resistor (part_num, power, resistance, composition) VALUES (${part_num}, ${power ?? null}, ${resistance}, ${composition ?? null});
+            `
+            break
+        case "capacitor": {
+            const { type: res_type, capacitance, temp_coeff } = body
+            await sql`
+            INSERT INTO capacitor (part_num, type, temp_coeff, capacitance) VALUES (${part_num}, ${res_type ?? null}, ${temp_coeff ?? null}, ${capacitance});
+            `
+            break
+        }
+        case "diode":
+            const { vforward, vreverse, capacitance } = body
+            await sql`
+            INSERT INTO diode (part_num, vforward, vreverse, capacitance) VALUES (${part_num}, ${vforward}, ${vreverse}, ${capacitance ?? null});
+            `
+            break
+    }
+
     return c.json(body, 200)
 })
-
-app.get('/components', async c => {
-    const res = await sql`SELECT component.part_num, *,
-        CASE
-            WHEN capacitor.part_num = component.part_num THEN 'Capacitor'
-            WHEN resistor.part_num = component.part_num THEN 'Resistor'
-            WHEN diode.part_num = component.part_num THEN 'Diode'
-            ELSE NULL END AS component_type
-    FROM component
-    LEFT JOIN capacitor USING (part_num)
-    LEFT JOIN resistor USING (part_num)
-    LEFT JOIN diode USING (part_num);`
-
-    return c.json(res);
-});
 
 app.delete("/:part_num", async (c) => {
     const { part_num } = c.req.param();
