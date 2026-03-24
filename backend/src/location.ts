d7fcaecf4f601f79fdc2b4df5c04a555e29a1702
Dorson T. <dtang08@students.cs.ubc.ca>
2026-03-23 19:08:55 -0700

    added join functionality with a placeholder table

--- backend/src/location.ts
+++ backend/src/location.ts
@@ -5,13 +5,30 @@ const app = new Hono()
 
 app.get('/', async c => {
     const fields = c.req.query('fields');
-    
+    const atr = c.req.query('atr');
+    const op = c.req.query('op');
+    const val = c.req.query('val');
+
     if (!fields) {
         return c.json(await sql`SELECT * FROM location`);
     }
     
     const columns = fields.split(',');
-    return c.json(await sql`SELECT DISTINCT ${sql(columns)} FROM location`);
+    // i spent a while trying to make op inline, but this is the only thing that worked
+    switch (op) {
+        case "e":
+            return c.json(await sql`SELECT DISTINCT ${sql(columns)} FROM location l NATURAL JOIN component c WHERE ${sql(`c.${atr}`)} = ${val}`);
+        case "lt":
+            return c.json(await sql`SELECT DISTINCT ${sql(columns)} FROM location l NATURAL JOIN component c WHERE ${sql(`c.${atr}`)} < ${val}`);
+        case "lte":
+            return c.json(await sql`SELECT DISTINCT ${sql(columns)} FROM location l NATURAL JOIN component c WHERE ${sql(`c.${atr}`)} <= ${val}`);
+        case "gt":
+            return c.json(await sql`SELECT DISTINCT ${sql(columns)} FROM location l NATURAL JOIN component c WHERE ${sql(`c.${atr}`)} > ${val}`);
+        case "gte":
+            return c.json(await sql`SELECT DISTINCT ${sql(columns)} FROM location l NATURAL JOIN component c WHERE ${sql(`c.${atr}`)} >= ${val}`);
+        case "ne":
+            return c.json(await sql`SELECT DISTINCT ${sql(columns)} FROM location l NATURAL JOIN component c WHERE ${sql(`c.${atr}`)} != ${val}`);
+    }
 })
 
 export default app
