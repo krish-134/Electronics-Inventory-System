1b2305d952a43ca49e265b04dab93b0c38189ea6
Dorson T. <dtang08@students.cs.ubc.ca>
2026-03-24 20:10:03 -0700

    project tab includes button to display projects that include every component

--- backend/src/project.ts
+++ backend/src/project.ts
@@ -4,7 +4,13 @@ import sql from './db/db'
 const app = new Hono()
 
 app.get('/', async c => {
-    return c.json(await sql`SELECT * FROM project`)
+    const division = c.req.query('has-all-components');
+
+    if (!division) return c.json(await sql`SELECT * FROM project`);
+
+    return c.json(await sql`SELECT name, purpose FROM project p WHERE NOT EXISTS ((SELECT c.part_num FROM component c) EXCEPT (SELECT i.component_part_num FROM includes i WHERE p.name = i.project_name))`);
 })
+
+
 
 export default app
--- frontend/app/routes/projects.tsx
+++ frontend/app/routes/projects.tsx
@@ -1,8 +1,56 @@
 import type React from "react"
+import Button from "@mui/material/Button";
+import { useState } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
 
 const Projects: React.FC = () => {
+    const [returned, setReturned] = useState<object[]>([]);
+    const [errorText, setErrorText] = useState<string>();
+
+    function request() {
+        fetch(`http://localhost:3000/project?has-all-components=true`)
+            .then(res => res.json())
+            .then(data => setReturned(data))
+            .then(_ => {if (!returned.length) setErrorText("None of your projects use all of your components!")})
+            .catch(err => console.error(err));
+    }
+
     return (
-        <>projects</>
+        <div className="flex flex-col my-3 space-y-6 w-1/2 pt-9">   
+            <p>
+                Click this button to see the projects that use all of your components!
+            </p>
+                
+            <Button onClick={request} sx={{ justifySelf: "left", width: 200, border: 1, mb: 3}}>Query!</Button>
+
+            {returned.length ? 
+                 <List
+                    sx={{
+                        width: '100%',
+                        maxWidth: 360,
+                        bgcolor: 'background.paper',
+                        position: 'relative',
+                        overflow: 'auto',
+                        maxHeight: 300,
+                        '& ul': { padding: 0 }
+                    }}
+                    >
+                    {returned.map((ret) => (
+                        <li>
+                            <ListItem>
+                                <ListItemText primary={`${Object.values(ret)[0]} : ${Object.values(ret)[1]}`} />
+                            </ListItem>
+                        </li>
+                    ))}
+                </List>
+            : 
+                <p className="text-red-400">
+                    {errorText}
+                </p>
+            }
+        </div>
     )
 }
 
