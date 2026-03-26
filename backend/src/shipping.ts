250de6dc21734c97fcc9b6a86733717d8c9ac3b3
Krishna Garcha <148072306+krish-134@users.noreply.github.com>
2026-03-25 20:37:29 -0700

    added backend for purchases

--- backend/src/shipping.ts
+++ backend/src/shipping.ts
@@ -11,8 +11,74 @@ app.get('/courier', async c => {
     return c.json(await sql`SELECT * FROM courier`)
 })
 
+// ----- PURCHASES ------
 app.get('/purchase', async c => {
     return c.json(await sql`SELECT * FROM purchase`)
+})
+
+app.post('/purchase', async c => { 
+    const body = await c.req.json()
+    const { 
+        order_number, 
+        price, 
+        tracking_code, 
+        date_placed, 
+        delivery_date, 
+        supplier, 
+        courier,
+    } = body
+    await sql`
+    INSERT INTO purchase (
+        order_number, 
+        price, 
+        tracking_code, 
+        date_placed, 
+        delivery_date, 
+        supplier, 
+        courier
+        ) VALUES (
+        ${order_number},
+        ${price},
+        ${tracking_code ?? null},
+        ${date_placed},
+        ${delivery_date ?? null},
+        ${supplier ?? null},
+        ${courier}
+        );`
+
+    return c.json(body, 200)
+})
+
+app.put('/purchase/:order_number', async c => {
+    const { order_number } = c.req.param();
+    const body = await c.req.json();
+    const { 
+        order_number : new_order_num, 
+        price, 
+        tracking_code, 
+        date_placed, 
+        delivery_date, 
+        supplier, 
+        courier,
+    } = body
+    await sql`
+        UPDATE purchase SET
+            order_number = ${new_order_num},
+            price = ${price},
+            tracking_code = ${tracking_code ?? null},
+            date_placed = ${date_placed},
+            supplier = ${supplier ?? null},
+            courier = ${courier}
+        WHERE order_number = ${order_number}`
+    return c.json(order_number, 200)
+})
+
+app.delete('purchase/:order_number', async c => {
+    const { order_number } = c.req.param();
+    await sql`
+        DELETE FROM purchase
+        WHERE order_number = ${order_number}`
+    return c.json(order_number, 200)
 })
 
 export default app
---
+++ frontend/app/shipping/SideBar.tsx
@@ -0,0 +1,102 @@
+import { ConstructionRounded, HomeRounded, LocalShippingRounded, LocationOnRounded, MemoryRounded, MoreVertRounded, SettingsRounded } from "@mui/icons-material"
+import { Avatar, Badge, badgeClasses, Box, drawerClasses, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Drawer as MUIDrawer, Stack, styled, Typography } from "@mui/material"
+import type React from "react"
+import { useState } from "react"
+import { Link, useLocation } from "react-router"
+
+const Drawer = styled(MUIDrawer)({
+    width: 240,
+    flexShrink: 0,
+    boxSizing: 'border-box',
+    mt: 10,
+    [`& .${drawerClasses.paper}`]: {
+        width: 240,
+        boxSizing: 'border-box'
+    }
+})
+
+interface MenuOption {
+    text: string
+    icon: React.ReactNode,
+    href: string
+}
+
+const TOP_OPTIONS: MenuOption[] = [
+    { text: "Home", icon: (<HomeRounded />), href: "/" },
+    { text: "Components", icon: (<MemoryRounded />), href: "/components" },
+    { text: "Projects", icon: (<ConstructionRounded />), href: "/projects" },
+    { text: "Locations", icon: (<LocationOnRounded />), href: "/locations" },
+    { text: "Shipping", icon: (<LocalShippingRounded />), href: "/shipping" },
+]
+
+const BOT_OPTIONS: MenuOption[] = [
+    { text: "Settings", icon: (<SettingsRounded />), href: "/settings" },
+]
+
+const SideBar: React.FC = () => {
+    const location = useLocation()
+    const [showError, setShowError] = useState(false)
+    return (
+        <Drawer variant="permanent"
+            sx={{
+                display: { xs: 'none', md: 'block' },
+                [`& .${drawerClasses.paper}`]: {
+                    backgroundColor: 'background.paper'
+                }
+            }}
+        >
+            <Box sx={{
+                overflow: 'auto',
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column'
+            }}>
+                <Stack sx={{ flexGrow: 1, p: 1, justifyContent: 'space-between' }}>
+                    <List dense>
+                        {TOP_OPTIONS.map((v, i) => (
+                            <ListItem key={i} disablePadding sx={{ display: 'block' }}>
+                                <Link to={v.href}>
+                                    <ListItemButton selected={location.pathname == v.href}>
+                                        <ListItemIcon>{v.icon}</ListItemIcon>
+                                        <ListItemText primary={v.text} />
+                                    </ListItemButton>
+                                </Link>
+                            </ListItem>
+                        ))}
+                    </List>
+                    <List dense>
+                        {BOT_OPTIONS.map((v, i) => (
+                            <ListItem key={i} disablePadding sx={{ display: 'block' }}>
+                                <Link to={v.href}>
+                                    <ListItemButton selected={location.pathname == v.href}>
+                                        <ListItemIcon>{v.icon}</ListItemIcon>
+                                        <ListItemText primary={v.text} />
+                                    </ListItemButton>
+                                </Link>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Stack>
+            </Box>
+            <Stack direction="row" sx={{ alignItems: 'center', p: 2, gap: 1, borderTop: '1px solid', borderColor: 'divider' }}>
+                <Avatar sizes="small" sx={{ width: 36, height: 36 }} />
+                <Box sx={{ mr: 'auto' }}>
+                    <Typography variant="body2" sx={{ fontWeight: 500, lineHeight: '16px' }}>
+                        First Last
+                    </Typography>
+                    <Typography variant="caption" sx={{ color: 'text.secondary' }}>
+                        student@ubc.ca
+                    </Typography>
+                </Box>
+
+                <Badge color="error" variant="dot" invisible={!showError} sx={{ [`& .${badgeClasses.badge}`]: { right: 2, top: 2 } }}>
+                    <IconButton size="small">
+                        <MoreVertRounded />
+                    </IconButton>
+                </Badge>
+            </Stack>
+        </Drawer>
+    )
+}
+
+export default SideBar
