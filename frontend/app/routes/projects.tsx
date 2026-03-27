0c59be26ca7c768f3d6f64295b633078495ca243
Dorson T. <dtang08@students.cs.ubc.ca>
2026-03-26 18:43:48 -0700

    projects with all components returns in display table

--- frontend/app/routes/projects.tsx
+++ frontend/app/routes/projects.tsx
@@ -1,9 +1,7 @@
 import type React from "react"
 import Button from "@mui/material/Button";
 import { useState } from "react";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
+import DisplayTable from "../components/DisplayTable";
 
 const Projects: React.FC = () => {
     const [returned, setReturned] = useState<object[]>([]);
@@ -18,39 +16,24 @@ const Projects: React.FC = () => {
     }
 
     return (
-        <div className="flex flex-col my-3 space-y-6 w-1/2 pt-9">   
-            <p>
-                Click this button to see the projects that use all of your components!
-            </p>
-                
-            <Button onClick={request} sx={{ justifySelf: "left", width: 200, border: 1, mb: 3}}>Query!</Button>
-
-            {returned.length ? 
-                 <List
-                    sx={{
-                        width: '100%',
-                        maxWidth: 360,
-                        bgcolor: 'background.paper',
-                        position: 'relative',
-                        overflow: 'auto',
-                        maxHeight: 300,
-                        '& ul': { padding: 0 }
-                    }}
-                    >
-                    {returned.map((ret) => (
-                        <li>
-                            <ListItem>
-                                <ListItemText primary={`${Object.values(ret)[0]} : ${Object.values(ret)[1]}`} />
-                            </ListItem>
-                        </li>
-                    ))}
-                </List>
-            : 
-                <p className="text-red-400">
-                    {errorText}
+        <>
+            <div className="flex flex-col my-3 space-y-6 w-1/2 pt-9">   
+                <p>
+                    Click this button to see the projects that use all of your components!
                 </p>
-            }
-        </div>
+                    
+                <Button onClick={request} sx={{ justifySelf: "left", width: 200, border: 1, mb: 3}}>Query!</Button>
+            </div>
+            <div className="w-3/4">
+                {returned.length > 0 ?
+                    <DisplayTable label={"Projects"} data={returned}/>
+                    :
+                    <p className="text-red-400">
+                        {errorText}
+                    </p>
+                }
+            </div>
+        </>
     )
 }
 
