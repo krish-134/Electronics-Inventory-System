7158afbb9afcbbbe1e54078191191c87f899d8dd
Dorson Tang <120761661+doruphin@users.noreply.github.com>
2026-03-26 18:43:48 -0700

    projects with all components returns in display table

--- backend/src/project.ts
+++ backend/src/project.ts
@@ -8,7 +8,7 @@ app.get('/', async c => {
 
     if (!division) return c.json(await sql`SELECT * FROM project`);
 
-    return c.json(await sql`SELECT name, purpose FROM project p WHERE NOT EXISTS ((SELECT c.part_num FROM component c) EXCEPT (SELECT i.component_part_num FROM includes i WHERE p.name = i.project_name))`);
+    return c.json(await sql`SELECT * FROM project p WHERE NOT EXISTS ((SELECT c.part_num FROM component c) EXCEPT (SELECT i.component_part_num FROM includes i WHERE p.name = i.project_name))`);
 })
 
 
