2f5c90c2c2215bb1a9619e957bbdc4f65fe1c6bb
Julian <jwrigh13@students.cs.ubc.ca>
2026-03-26 18:55:39 -0700

    feat: drag functinoality (no drop)

--- frontend/app/components/data/Locations.tsx
+++ frontend/app/components/data/Locations.tsx
@@ -1,8 +1,47 @@
 import type React from "react"
 import { Box, Button, Grid, Icon, Stack, Typography } from "@mui/material"
 import { Location } from '../../types'
-import { useEffect, useMemo, useState } from "react"
+import { PropsWithChildren, useEffect, useMemo, useState } from "react"
 import { Add, DragHandle, DragIndicator } from "@mui/icons-material"
+import { DndProvider, useDrag, useDrop } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
+const ComponentItem: React.FC<PropsWithChildren<{}>> = ({ children }) => {
+    const [{ isDragging }, drag, dragPreview] = useDrag(() => ({
+        type: 'COMPONENT',
+        collect: (monitor) => ({
+            isDragging: monitor.isDragging()
+        })
+    }))
+
+    return (
+        <div ref={dragPreview} style={{ opacity: isDragging ? 0.5 : 1 }}>
+            <Stack direction="row" gap={1} sx={{ alignItems: 'center', alignContent: 'center' }}>
+                <div role="Handle" ref={drag}><DragIndicator sx={{ cursor: "grab" }} fontSize="small" /></div>
+                {children}
+            </Stack>
+        </div>
+    )
+}
+
+const ComponentList: React.FC<PropsWithChildren<Location>> = ({ children, ...location }) => {
+    const [{ canDrop, isOver }, drop] = useDrop(() => ({
+        accept: 'COMPONENT',
+        collect: (monitor) => ({
+            isOver: monitor.isOver(),
+            canDrop: monitor.canDrop()
+        })
+    }))
+
+    return (
+        <Stack>
+            <Typography variant="subtitle2">{location.position}</Typography>
+            <Box ref={drop} role={'Dustbin'} sx={{ bgcolor: isOver ? 'background.default' : '', p: 1, borderRadius: 1 }}>
+                {children}
+            </Box>
+        </Stack>
+    )
+}
 
 const Locations: React.FC = () => {
     const [locations, setLocations] = useState<Location[]>([])
@@ -38,46 +77,46 @@ const Locations: React.FC = () => {
     }, [locations])
 
     return (
-        <Grid container spacing={2} sx={{ width: "100%" }}>
-            {[...byFacility.entries()].map(([facility, locs]) => (
-                <Grid key={facility} sx={{ border: "1px solid", borderColor: "divider", p: 4, borderRadius: 2, width: "100%" }}>
-                    <Typography variant="h6">{facility}</Typography>
-                    <Stack direction="column" gap={2} sx={{ mt: 2 }}>
-                        {[...locs.entries()].map(([display_name, locs]) => (
-                            <Box key={display_name} sx={{ border: "1px solid", borderColor: "background.paper", p: 4, borderRadius: 2, width: "100%" }}>
-                                <Typography variant="subtitle1">{display_name}</Typography>
-                                <Grid container size={3} sx={{ width: "100%", mt: 1 }}>
-                                    {locs.map(loc => (
-                                        <Grid key={loc.position} sx={{ bgcolor: "background.paper", p: 2, borderRadius: 2 }} size={3}>
-                                            <Typography variant="subtitle2">{loc.position}</Typography>
-                                            <Stack direction="column" gap={0.5} sx={{ mt: 1 }}>
-                                                {componentLocations[facility]?.[loc.storage_name]?.map(c => (
-                                                    <Stack direction="row" gap={1} sx={{ alignItems: 'center', alignContent: 'center' }}>
-                                                        <DragIndicator fontSize="small" />
-                                                        <Typography variant="caption">
-                                                            {c.part_num}
-                                                        </Typography>
-                                                    </Stack>
-                                                )) ?? (
-                                                        <Typography variant="caption">No components...</Typography>
-                                                    )}
-                                            </Stack>
-                                        </Grid>
-                                    ))}
-                                </Grid>
-                            </Box>
-                        ))}
-                    </Stack>
+        <DndProvider backend={HTML5Backend}>
+            <Grid container spacing={2} sx={{ width: "100%" }}>
+                {[...byFacility.entries()].map(([facility, locs]) => (
+                    <Grid key={facility} sx={{ border: "1px solid", borderColor: "divider", p: 4, borderRadius: 2, width: "100%" }}>
+                        <Typography variant="h6">{facility}</Typography>
+                        <Stack direction="column" gap={2} sx={{ mt: 2 }}>
+                            {[...locs.entries()].map(([display_name, locs]) => (
+                                <Box key={display_name} sx={{ border: "1px solid", borderColor: "background.paper", p: 4, borderRadius: 2, width: "100%" }}>
+                                    <Typography variant="subtitle1">{display_name}</Typography>
+                                    <Grid container size={3} sx={{ width: "100%", mt: 1 }}>
+                                        {locs.map(loc => (
+                                            <Grid key={loc.position} sx={{ bgcolor: "background.paper", p: 2, borderRadius: 2 }} size={3}>
+                                                <ComponentList {...loc}>
+                                                    {componentLocations[facility]?.[loc.storage_name]?.map(c => (
+                                                        <ComponentItem>
+                                                            <Typography variant="caption">
+                                                                {c.part_num}
+                                                            </Typography>
+                                                        </ComponentItem>
+                                                    )) ?? (
+                                                            <Typography variant="caption">No components...</Typography>
+                                                        )}
+                                                </ComponentList>
+                                            </Grid>
+                                        ))}
+                                    </Grid>
+                                </Box>
+                            ))}
+                        </Stack>
+                    </Grid>
+                ))}
+                <Grid sx={{
+                    border: "1px solid", borderColor: "divider", borderRadius: 2, width: "100%", display: "flex", flexDirection: "row", justifyContent: "center"
+                }}>
+                    <Button sx={{ width: "100%", height: "100%", p: 4 }} disableRipple>
+                        <Add sx={{ color: "text.primary" }} />
+                    </Button>
                 </Grid>
-            ))}
-            <Grid sx={{
-                border: "1px solid", borderColor: "divider", borderRadius: 2, width: "100%", display: "flex", flexDirection: "row", justifyContent: "center"
-            }}>
-                <Button sx={{ width: "100%", height: "100%", p: 4 }} disableRipple>
-                    <Add sx={{ color: "text.primary" }} />
-                </Button>
             </Grid>
-        </Grid>
+        </DndProvider >
     )
 }
 
