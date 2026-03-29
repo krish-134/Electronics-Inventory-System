7f30c5d96fc7ad9675caad243b2b3b8765d3b039
Krishna Garcha <148072306+krish-134@users.noreply.github.com>
2026-03-28 22:12:04 -0700

    feat: implemented frontend in CouriersTable

--- frontend/app/routes/shipping.tsx
+++ frontend/app/routes/shipping.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import PurchasesTable from "../shipping/data/PurchasesTable"
 import SuppliersTable from "../shipping/data/SuppliersTable"
-// import CouriersTable from "../shipping/data/CouriersTable"
+import CouriersTable from "../shipping/data/CouriersTable"
 import { Stack } from "@mui/material"
 
 const Shipping: React.FC = () => {
@@ -10,7 +10,7 @@ const Shipping: React.FC = () => {
         <Stack direction="column" gap={2}>
             <PurchasesTable />
             <SuppliersTable />
-            {/* <CouriersTable /> */}
+            <CouriersTable />
         </Stack>
     )
 }
